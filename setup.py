c3fd6d4fc5e2eae42bda88dfe20f0aa330327d66
Jorge Matricali <jorgematricali@gmail.com>
2015-01-02 16:08:15 -0300

    + install as command line tool using pip

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 from distutils.core import setup
 
 setup(
-  name='brutekrag',
-  packages=['brutekrag'],
-  version='0.1.1',
-  description='brute force OpenSSH using dictionary attack',
-  author='Jorge Matricali',
-  author_email='jorgematricali@gmail.com',
-  license='MIT',
-  url='https://github.com/jorge-matricali/brutekrag',
-  download_url='https://github.com/jorge-matricali/brutekrag/archive/v0.1.1.tar.gz',
-  scripts=['bin/brutekrag'],
-  keywords=['ssh', 'brute force', 'ethical hacking', 'pentesting'],
-  classifiers=[],
+    name='brutekrag',
+    packages=['brutekrag'],
+    version='0.1.3',
+    description='brute force OpenSSH using dictionary attack',
+    author='Jorge Matricali',
+    author_email='jorgematricali@gmail.com',
+    license='MIT',
+    url='https://github.com/jorge-matricali/brutekrag',
+    download_url='https://github.com/jorge-matricali/brutekrag/archive/v0.1.2.tar.gz',
+    scripts=['bin/brutekrag'],
+    keywords=['ssh', 'brute force', 'ethical hacking', 'pentesting'],
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ),
+    install_requires=['paramiko==1.5.2',
+                      'argparse==1.2.2']
 )
