d66f5f52da2f7f235b51d2855400bc70f2447642
Jorge Matricali <jorgematricali@gmail.com>
2017-06-06 00:49:36 -0300

    + Release v0.2.0

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='brutekrag',
     packages=['brutekrag'],
-    version='0.1.9',
+    version='0.2.0',
     description='brute force OpenSSH using dictionary attack',
     author='Jorge Matricali',
     author_email='jorgematricali@gmail.com',
     license='MIT',
     url='https://github.com/jorge-matricali/brutekrag',
-    download_url='https://github.com/jorge-matricali/brutekrag/archive/v0.1.8.tar.gz',
+    download_url='https://github.com/jorge-matricali/brutekrag/archive/v0.2.0.tar.gz',
     scripts=['bin/brutekrag'],
     keywords=['ssh', 'brute force', 'ethical hacking', 'pentesting'],
     classifiers=(
